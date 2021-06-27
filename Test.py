86e6459e7b3eb57845d767147a9b44dfaf002cba
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2021-06-26 23:02:04 -0400

    Update Test.py
    since it is gui based now, just change it to start the gui

--- Test.py
+++ Test.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-" THis is the test clinet for the landmine game "
+" This is the test clinet for the sudoku solver "
 
 __author__ = "Austin Wei"
 
-import Board, check
+import Board, Temp_board, GUI
 
-b = [
-    [0, 2, 0, 6, 0, 8, 0, 0, 0],
-    [5, 8, 0, 0, 0, 9, 7, 0, 0],
-    [0, 0, 0, 0, 4, 0, 0, 0, 0],
-    [3, 7, 0, 0, 0, 0, 5, 0, 0],
-    [6, 0, 0, 0, 0, 0, 0, 0, 4],
-    [0, 0, 8, 0, 0, 0, 0, 1, 3],
-    [0, 0, 0, 0, 2, 0, 0, 0, 0],
-    [0, 0, 9, 8, 0, 0, 0, 3, 6],
-    [0, 0, 0, 3, 0, 6, 0, 9, 0]
-]
 
-board = Board.Board(b)
-
-print(board.check_valid(4, 8, 1))
-print(board.check_valid(4, 8, 2))
-print(board.check_valid(4, 8, 3))
-print(board.check_valid(4, 8, 4))
-print(board.check_valid(4, 8, 5))
-print(board.check_valid(4, 8, 6))
-print(board.check_valid(4, 8, 7))
-print(board.check_valid(4, 8, 8))
-print(board.check_valid(4, 8, 9))
+GUI.start()
