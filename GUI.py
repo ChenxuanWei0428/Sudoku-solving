549896ed7cee11cecb7ea5a0c263d31d508f0f33
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2021-07-02 01:11:50 -0400

    Update GUI.py
    Modify to a class to allowed calls and rearrange methods for call different levels.

--- GUI.py
+++ GUI.py
@@ -5,144 +5,187 @@
 
 __author__ = "Austin Wei"
 
-import Board, tkinter, Temp_board
+import Board, tkinter, Temp_board, time
 
-main_board = tkinter.Tk()
-main_board.title("Sudoku Solver")
-main_board.geometry('750x700')
+class GUI :
 
-current_board = Temp_board.b
-# the board that is showed in the game
-init_board = Board.Board(current_board)
+    def __init__(self):
+        self.main_board = tkinter.Tk()
 
-# this is a 2d list of tkinter.Entry
-sudoku_board = []
+        # the two frame, one is for the board, other for the bottoms
+        self.board_frame = tkinter.Frame(self.main_board, width = 200, height = 200)
+        self.bottom_frame = tkinter.Frame(self.main_board)
 
-# the two frame, one is for the board, other for the bottoms
-board_frame = tkinter.Frame(main_board, width = 200, height = 200)
-bottom_frame = tkinter.Frame(main_board)
+        #edit main_board
+        self.main_board.title("Sudoku Solver")
+        self.main_board.geometry('750x700')
 
-# the status_text
-status_text = tkinter.Label(bottom_frame, text = "Welcome to Sudoku Game!")
+        self.current_board = Temp_board.level[0]
+        # the board that is showed in the game
+        self.level_board = Board.Board(self.current_board)
 
+        # this is a 2d list of tkinter.Entry
+        self.sudoku_entry = []
 
-'''
-Purpose: input_row(board_row) Return a 1D array of entry in tk which include one row of the board
-Contract: list[] -> list[]
-'''
-def input_row (board_row):
-    temp_row = []
-    l = len(board_row)
-    for i in range(l):
-        temp_row.append(tkinter.Entry(board_frame, font = 10, width = 3))
-        if board_row[i] != 0:
-            temp_row[i].insert(0, board_row[i])
-    return temp_row
+        # the two frame, one is for the board, other for the bottoms
+        self.board_frame = tkinter.Frame(self.main_board, width = 200, height = 200)
+        bottom_frame = tkinter.Frame(self.main_board)
 
+        # the status_text
+        self.status = tkinter.StringVar()
+        self.status.set("Welcome to Sudoku Game")
+        self.status_text = tkinter.Label(self.bottom_frame, textvariable=self.status)
 
-'''
-Purpose: input_board(board) Modify the 2D array of entry to make a entry of the board
-effect: modify sudoku_board
-Contarct: Board -> 
-'''
-def input_board (board):
-    for board_row in board.board:
-        sudoku_board.append(input_row(board_row))
+        self.submit_botton = tkinter.Button(self.bottom_frame, text = "submit!", command = (lambda: self.submit()), height = 1, width = 1)
+        self.solve_botton = tkinter.Button(self.bottom_frame, text = "solve!", command = (lambda: self.solve()), height = 1, width = 1)
 
 
-'''
-Purpose: add_to_screen(loe) Put everything is the list of entry to the screen
-effect: modify the main_board
-'''
-def add_to_screen(loe):
-    l = len(loe)
-    for row in range(l):
-        for entry in range(l):
-            sudoku_board[row][entry].grid(row = row, column = entry, padx = 10, pady = 10, ipadx = 10, ipady = 10)
+    '''
+    Purpose: init_row(board_row) Return a 1D array of entry in tk which include one row of the board
+    Contract: list[] -> list[]
+    '''
+    def init_row (self, row_len):
+        temp_row = []
+        for i in range(row_len):
+            temp_row.append(tkinter.Entry(self.board_frame, font = 10, width = 3))
+        return temp_row
 
 
-'''
-Purpose: solve() is a tkinter command for solve the sudoku
-effect: edit the entrys and displace the solved board
-'''
-def solve():
-    l = len(init_board.board)
-    for row in range(l):
-        for entry in range(l):
-            sudoku_board[row][entry].delete(0, 'end')
-            sudoku_board[row][entry].insert(0, init_board.board[row][entry])
-
-    status_text = tkinter.Label(bottom_frame, text = "The solution is presented")
-    status_text.grid(row = 1, column = 3, padx = 10, pady = 10, ipadx = 10, ipady = 10)
+    '''
+    Purpose: input_board(board) Modify the 2D array of entry to make a entry of the board
+    effect: modify sudoku_entry
+    Contarct: Board -> 
+    '''
+    def init_entry_array (self):
+        l = self.level_board.length
+        for board_row in range(l):
+            self.sudoku_entry.append(self.init_row(l))
 
 
-''' 
-Purpose: submit() is a tkinter command to check if the answer matches the given answer
-effect: edit the entrys and indicate weather it is true or not
-'''
-def submit():
-    correct = True
-    empty = False
-    l = len(init_board.board)
-    for row in range(l):
-        for entry in range(l):
-            # If it is not blank and it does not match the solution board, remove it
-            if (len(sudoku_board[row][entry].get()) != 0 and int(sudoku_board[row][entry].get()) != init_board.board[row][entry]):
-                correct = False
-                sudoku_board[row][entry].delete(0, 'end')
-            elif (len(sudoku_board[row][entry].get()) == 0 ):
-                empty = True
-    
-    status_text.pack_forget()
-    submit_label = tkinter.Label(bottom_frame)
-    if not correct:
-        sumbit_label = tkinter.Label(bottom_frame, text = "Please try again")
-    elif (empty):
-        sumbit_label = tkinter.Label(bottom_frame, text = "You still have empty space to fill!")
-    else:
-        submit_label = tkinter.Label(bottom_frame, text = "Great Job!")
-    submit_label.grid(row = 1, column = 3, padx = 10, pady = 10, ipadx = 10, ipady = 10)
+    '''
+    Purpose: add_to_screen(loe) Put everything is the list of entry to the screen
+    effect: modify the main_board
+    '''
+    def add_to_screen(self):
+        l = len(self.sudoku_entry)
+        for row in range(l):
+            for entry in range(l):
+                self.sudoku_entry[row][entry].grid(row = row, column = entry, padx = 10, pady = 10, ipadx = 10, ipady = 10)
 
 
-'''
-Purpose: add_solve_botton_to_screen() add the solve botton to the screen
-effect: modify main_board()
-'''
-def add_solve_botton_to_screen():
-    solve_botton = tkinter.Button(bottom_frame, text = "solve!", command = (lambda: solve()), height = 1, width = 1)
-    solve_botton.grid(row = 1, column = 1, padx = 10, pady = 10, ipadx = 10, ipady = 10)
+    '''    
+    Purpose input_level() input the level_board to the current_board
+    effect: modify sudoku_entry()
+    '''
+    def input_level(self):
+        l = len(self.sudoku_entry)
+        for row in range(l):
+            for cell in range(l):
+                if (self.level_board.board[row][cell] != 0):
+                    self.sudoku_entry[row][cell].insert(0, self.level_board.board[row][cell])
+        self.level_board.solve()
 
 
-'''   
-Purpsoe: add_submit button to screen() add the submit botton for user to the screen
-effect: modify main_board()
-'''
-def add_submit_botton_to_screen():
-    submit_botton = tkinter.Button(bottom_frame, text = "submit!", command = (lambda: submit()), height = 1, width = 1)
-    submit_botton.grid(row = 1, column = 2, padx = 10, pady = 10, ipadx = 20, ipady = 10)
+    '''
+    Purpose: solve() is a tkinter command for solve the sudoku
+    effect: edit the entrys and displace the solved board
+    '''
+    def solve(self):
+        l = len(self.level_board.board)
+        for row in range(l):
+            for entry in range(l):
+                self.sudoku_entry[row][entry].delete(0, 'end')
+                self.sudoku_entry[row][entry].insert(0, self.level_board.board[row][entry])
+
+        self.status.set("The solution is presented")
 
 
-'''
-Purpose: add_status_text_to_screen() add a status bar to the screen, shows, weather you win, or still need to try or solved it
-effect: modify main_board()
-'''
-def add_status_text_to_screen():
-    status_text.grid(row = 1, column = 3, padx = 10, pady = 10, ipadx = 10, ipady = 10)
+    ''' 
+    Purpose: submit() is a tkinter command to check if the answer matches the given answer
+    effect: edit the entrys and indicate weather it is true or not
+    '''
+    def submit(self):
+        correct = True
+        empty = False
+        l = len(self.level_board.board)
+        for row in range(l):
+            for entry in range(l):
+                # If it is not blank and it does not match the solution board, remove it
+                if (len(self.sudoku_entry[row][entry].get()) != 0 and int(self.sudoku_entry[row][entry].get()) != self.level_board.board[row][entry]):
+                    correct = False
+                    self.sudoku_entry[row][entry].delete(0, 'end')
+                elif (len(self.sudoku_entry[row][entry].get()) == 0 ):
+                    empty = True
+        
+        if not correct:
+            self.status.set("Please try again!")
+        elif (empty):
+            self.status.set("You still have empty space to fill!")
+        else:
+            self.status.set("Great job!")
+        
+
+    def next_level(self):
+        pass
+
+    '''
+    Purpose: add_solve_botton_to_screen() add the solve botton to the screen
+    effect: modify main_board()
+    '''
+    def add_solve_botton_to_screen(self):
+        self.solve_botton.grid(row = 1, column = 1, padx = 10, pady = 10, ipadx = 10, ipady = 10)
 
 
+    '''   
+    Purpsoe: add_submit button to screen() add the submit botton for user to the screen
+    effect: modify main_board()
+    '''
+    def add_submit_botton_to_screen(self):
+        self.submit_botton.grid(row = 1, column = 2, padx = 10, pady = 10, ipadx = 20, ipady = 10)
 
-#start the Sudolu solver program
-def start():
 
-    input_board(init_board)
-    
-    add_to_screen(sudoku_board)
-    add_solve_botton_to_screen()
-    add_submit_botton_to_screen()
-    add_status_text_to_screen()
-    board_frame.pack()
-    bottom_frame.pack()
-    
-    init_board.solve()
-    
-    main_board.mainloop()
+    '''
+    Purpose: add_status_text_to_screen() add a status bar to the screen, shows, weather you win, or still need to try or solved it
+    effect: modify main_board()
+    '''
+    def add_status_text_to_screen(self):
+        self.status_text.grid(row = 1, column = 3, padx = 10, pady = 10, ipadx = 10, ipady = 10)
+
+
+    '''
+    Purpose: delay_five_second is used after finish a level, delay and then do the next step
+    effect: effect output
+    '''
+    def delay_second(self, second):
+        pass
+
+    #start the Sudolu solver program
+    def start(self):
+        #init the game
+        self.init_entry()
+        self.init_game()
+        self.input_level()
+        self.main_board.mainloop()
+
+
+    '''
+    Purpose: initlize the entry and ready for inputting level
+    effect: modify entry
+    '''
+    def init_entry(self):
+        self.init_entry_array()
+        self.add_to_screen()
+        self.board_frame.pack()
+
+
+    '''
+    Purpose: initlize the bottom and status bar
+    effect: modify entry
+    '''
+    def init_game(self):
+        self.add_solve_botton_to_screen()
+        self.add_submit_botton_to_screen()
+        self.add_solve_botton_to_screen()
+        self.add_submit_botton_to_screen()
+        self.add_status_text_to_screen()
+        self.bottom_frame.pack()
