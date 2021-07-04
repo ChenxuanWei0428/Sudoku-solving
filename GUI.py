e5bae2ad45e4b71fa0325df4c92ca374d27f5531
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2021-07-03 21:37:56 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -20,8 +20,10 @@ class GUI :
         self.main_board.title("Sudoku Solver")
         self.main_board.geometry('750x700')
 
-        self.current_board = Temp_board.level[0]
+
         # the board that is showed in the game
+        self.boardset = Temp_board.next()
+        self.current_board = self.boardset()
         self.level_board = Board.Board(self.current_board)
 
         # this is a 2d list of tkinter.Entry
@@ -40,6 +42,76 @@ class GUI :
         self.solve_botton = tkinter.Button(self.bottom_frame, text = "solve!", command = (lambda: self.solve()), height = 1, width = 1)
 
 
+    '''    
+    Purpose input_level() input the level_board to the entrys
+    effect: modify sudoku_entry
+    '''
+    def input_level(self,):
+        l = len(self.sudoku_entry)
+        for row in range(l):
+            for cell in range(l):
+                if (self.level_board.board[row][cell] != 0):
+                    self.sudoku_entry[row][cell].insert(0, self.level_board.board[row][cell])
+        self.level_board.solve()
+
+
+    '''
+    Purpose: solve() is a tkinter command for solve the sudoku, show the solution the input the next level
+    effect: edit the entrys and displace the solved board
+    '''
+    def solve(self):
+        l = len(self.level_board.board)
+        for row in range(l):
+            for entry in range(l):
+                self.sudoku_entry[row][entry].delete(0, 'end')
+                self.sudoku_entry[row][entry].insert(0, self.level_board.board[row][entry])
+
+        self.status.set("The solution is presented")
+        # need to learn why this is not working
+        #time.sleep(5)
+        self.next_level()
+
+
+    ''' 
+    Purpose: submit() is a tkinter command to check if the answer matches the given answer
+    effect: edit the entrys and indicate weather it is true or not
+    '''
+    def submit(self):
+        correct = True
+        empty = False
+        l = len(self.level_board.board)
+        for row in range(l):
+            for entry in range(l):
+                # If it is not blank and it does not match the solution board, remove it
+                if (len(self.sudoku_entry[row][entry].get()) != 0 and int(self.sudoku_entry[row][entry].get()) != self.level_board.board[row][entry]):
+                    correct = False
+                    self.sudoku_entry[row][entry].delete(0, 'end')
+                elif (len(self.sudoku_entry[row][entry].get()) == 0 ):
+                    empty = True
+        
+        if not correct:
+            self.status.set("Please try again!")
+        elif (empty):
+            self.status.set("You still have empty space to fill!")
+        else:
+            self.status.set("Great job!")
+
+
+    '''
+    Purpose: Put the game into next level
+    effect: modify entrys
+    '''
+    def next_level(self):
+        l = len(self.sudoku_entry)
+        self.status.set("Welcome to next level")
+        for row in range(l):
+            for entry in range(l):
+                self.sudoku_entry[row][entry].delete(0, 'end')
+        self.current_board = self.boardset()
+        self.level_board = Board.Board(self.current_board)
+        self.input_level()
+
+     
     '''
     Purpose: init_row(board_row) Return a 1D array of entry in tk which include one row of the board
     Contract: list[] -> list[]
@@ -72,62 +144,7 @@ class GUI :
             for entry in range(l):
                 self.sudoku_entry[row][entry].grid(row = row, column = entry, padx = 10, pady = 10, ipadx = 10, ipady = 10)
 
-
-    '''    
-    Purpose input_level() input the level_board to the current_board
-    effect: modify sudoku_entry()
-    '''
-    def input_level(self):
-        l = len(self.sudoku_entry)
-        for row in range(l):
-            for cell in range(l):
-                if (self.level_board.board[row][cell] != 0):
-                    self.sudoku_entry[row][cell].insert(0, self.level_board.board[row][cell])
-        self.level_board.solve()
-
-
-    '''
-    Purpose: solve() is a tkinter command for solve the sudoku
-    effect: edit the entrys and displace the solved board
-    '''
-    def solve(self):
-        l = len(self.level_board.board)
-        for row in range(l):
-            for entry in range(l):
-                self.sudoku_entry[row][entry].delete(0, 'end')
-                self.sudoku_entry[row][entry].insert(0, self.level_board.board[row][entry])
-
-        self.status.set("The solution is presented")
-
-
-    ''' 
-    Purpose: submit() is a tkinter command to check if the answer matches the given answer
-    effect: edit the entrys and indicate weather it is true or not
-    '''
-    def submit(self):
-        correct = True
-        empty = False
-        l = len(self.level_board.board)
-        for row in range(l):
-            for entry in range(l):
-                # If it is not blank and it does not match the solution board, remove it
-                if (len(self.sudoku_entry[row][entry].get()) != 0 and int(self.sudoku_entry[row][entry].get()) != self.level_board.board[row][entry]):
-                    correct = False
-                    self.sudoku_entry[row][entry].delete(0, 'end')
-                elif (len(self.sudoku_entry[row][entry].get()) == 0 ):
-                    empty = True
-        
-        if not correct:
-            self.status.set("Please try again!")
-        elif (empty):
-            self.status.set("You still have empty space to fill!")
-        else:
-            self.status.set("Great job!")
-        
-
-    def next_level(self):
-        pass
-
+    
     '''
     Purpose: add_solve_botton_to_screen() add the solve botton to the screen
     effect: modify main_board()
@@ -152,18 +169,13 @@ class GUI :
         self.status_text.grid(row = 1, column = 3, padx = 10, pady = 10, ipadx = 10, ipady = 10)
 
 
-    '''
-    Purpose: delay_five_second is used after finish a level, delay and then do the next step
-    effect: effect output
-    '''
-    def delay_second(self, second):
-        pass
-
     #start the Sudolu solver program
     def start(self):
         #init the game
         self.init_entry()
         self.init_game()
+
+
         self.input_level()
         self.main_board.mainloop()
 
@@ -183,8 +195,6 @@ class GUI :
     effect: modify entry
     '''
     def init_game(self):
-        self.add_solve_botton_to_screen()
-        self.add_submit_botton_to_screen()
         self.add_solve_botton_to_screen()
         self.add_submit_botton_to_screen()
         self.add_status_text_to_screen()
